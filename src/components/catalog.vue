f80ad50f97beaaf2b13f89cd29aa5f72dcca0705
Artur Orlov <aaorlov@scid.ru>
2026-05-21 16:24:02 +0300

    fanamo/

--- src/components/catalog.vue
+++ src/components/catalog.vue
@@ -38,13 +38,13 @@
   const filters = ref({ all: true })
 
   const cards = ref([
-    { id: 1, images: ['/public/images/basket1.webp', '/public/images/basket2.webp', '/public/images/basket3.webp'], name: 'Корзина для игрушек', price: '1200', link: '', category: 'Корзина для игрушек' },
+    { id: 1, images: ['/funamo/images/basket1.webp', '/funamo/images/basket2.webp', '/funamo/images/basket3.webp'], name: 'Корзина для игрушек', price: '1200', link: '', category: 'Корзина для игрушек' },
     { id: 2, images: ['/noimage.webp'], name: 'Корзина для хранения', price: '3040', link: '', category: 'Корзина для хранения' },
     { id: 3, images: ['/noimage.webp'], name: 'Защитная накладка для дома', price: '12100', link: '', category: 'Защитная накладка для дома' },
     { id: 4, images: ['/noimage.webp'], name: 'Набор для праздника', price: '3040', link: '', category: 'Набор для праздника' },
     { id: 5, images: ['/noimage.webp'], name: 'Игровая палатка', price: '1200', link: '', category: 'Игровая палатка' },
     { id: 6, images: ['/noimage.webp'], name: 'Нагрудник', price: '3040', link: '', category: 'Нагрудник' },
-    { id: 7, images: ['/public/images/carpet-white1.webp', '/public/images/carpet-white2.webp', '/public/images/carpet-white3.webp'], name: 'Коврик детский', price: '3040', link: '', category: 'Коврик детский' },
+    { id: 7, images: ['/funamo/images/carpet-white1.webp', '/funamo/images/carpet-white2.webp', '/funamo/images/carpet-white3.webp'], name: 'Коврик детский', price: '3040', link: '', category: 'Коврик детский' },
     { id: 8, images: ['/noimage.webp'], name: 'Корзина для игрушек розовая', price: '1500', link: '', category: 'Корзина для игрушек' },
     { id: 9, images: ['/noimage.webp'], name: 'Игровая палатка с звездами', price: '2500', link: '', category: 'Игровая палатка' },
   ])
